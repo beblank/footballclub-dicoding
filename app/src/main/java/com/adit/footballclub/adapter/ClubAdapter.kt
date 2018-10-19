8a07972a9f001d7243dd87076c9ec28f3103b7a2
adit <kulik2@ymail.com>
2018-10-19 16:13:45 +0700

    create club rv layout

--- app/src/main/java/com/adit/footballclub/adapter/ClubAdapter.kt
+++ app/src/main/java/com/adit/footballclub/adapter/ClubAdapter.kt
@@ -1,3 +1,26 @@
 package com.adit.footballclub.adapter
 
-class ClubAdapter
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import com.adit.footballclub.entity.Events
+
+class ClubAdapter(val eventList:List<Events>):RecyclerView.Adapter<ClubAdapter.ViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+    class ViewHolder(view:View):RecyclerView.ViewHolder(view) {
+
+    }
+
+}
