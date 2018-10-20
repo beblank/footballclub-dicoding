f762c30ccc084672707b095a8367faf9de9c8b85
adit <kulik2@ymail.com>
2018-10-20 09:32:53 +0700

    detail using rv

---
+++ app/src/main/java/com/adit/footballclub/adapter/DetailAdapter.kt
@@ -0,0 +1,48 @@
+package com.adit.footballclub.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.RecyclerView.ViewHolder
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.adit.footballclub.entity.Events
+import com.adit.footballclub.R
+import com.adit.footballclub.Utils.Utils
+import kotlinx.android.synthetic.main.match_item.view.*
+
+class DetailAdapter(val listAttrib:Array<String>, val event:Events): RecyclerView.Adapter<ViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        var view:ViewHolder= HeaderViewHolder(LayoutInflater.from(parent.context).inflate(
+                R.layout.team_detail_item, parent, false))
+        when(viewType){
+            0 -> view = HeaderViewHolder(LayoutInflater.from(parent.context).inflate(
+                    R.layout.match_detail_header_item, parent, false))
+        }
+        return view
+    }
+
+    override fun getItemCount() = 1
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        Log.d("dodol", "${holder.itemViewType}")
+        when(holder.itemViewType){
+            0 -> {
+                holder as HeaderViewHolder
+                holder.bind(event)
+            }
+        }
+    }
+
+
+    class HeaderViewHolder(itemView:View):ViewHolder(itemView) {
+        fun bind(event: Events){
+            itemView.txt_date.text  = Utils.dateStringConverter(event.dateStr)
+            itemView.txt_home_name.text = event.nameHomeTeam
+            itemView.txt_home_score.text = event.homeScore
+            itemView.txt_away_name.text = event.nameAwayTeam
+            itemView.txt_away_score.text = event.awayScore
+        }
+    }
+
+}
--- app/src/main/java/com/adit/footballclub/ui/activity/DetailActivity.kt
+++ app/src/main/java/com/adit/footballclub/ui/activity/DetailActivity.kt
@@ -2,9 +2,13 @@ package com.adit.footballclub.ui.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import com.adit.footballclub.R
 import com.adit.footballclub.Utils.Const
+import com.adit.footballclub.adapter.DetailAdapter
 import com.adit.footballclub.entity.Events
+import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : AppCompatActivity() {
 
@@ -13,5 +17,7 @@ class DetailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_detail)
 
         val event = intent.getParcelableExtra<Events>(Const.event)
+        rvDetailMatch.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        rvDetailMatch.adapter = DetailAdapter(resources.getStringArray(R.array.attrib), event)
     }
 }
