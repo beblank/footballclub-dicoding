ee7455e7f77e622ff3ff43a810094a90584dbf88
adit <kulik2@ymail.com>
2018-10-31 10:37:19 +0700

    detail activity view model test

--- app/src/test/java/com/adit/footballclub/utils/UtilsTest.kt
+++ app/src/test/java/com/adit/footballclub/utils/UtilsTest.kt
@@ -1,13 +1,13 @@
 package com.adit.footballclub.utils
 
-import org.junit.Assert
 import org.junit.Test
+import kotlin.test.assertEquals
 
 class UtilsTest{
         @Test
         fun testToDateString(){
             val date = "28/02/2018"
-            Assert.assertEquals("Wed, 28 Feb 2018", Utils.dateStringConverter(date))
+            assertEquals("Wed, 28 Feb 2018", Utils.dateStringConverter(date))
         }
 
 }
---
+++ app/src/test/java/com/adit/footballclub/viewmodel/DetailActivityViewModelTest.kt
@@ -0,0 +1,60 @@
+package com.adit.footballclub.viewmodel
+
+import android.arch.core.executor.testing.InstantTaskExecutorRule
+import android.arch.lifecycle.ViewModelProviders
+import android.support.v4.app.FragmentActivity
+import com.adit.footballclub.BaseTest
+import com.adit.footballclub.utils.RxImmediateSchedulerRule
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
+
+@RunWith(RobolectricTestRunner::class)
+class DetailActivityViewModelTest: BaseTest(){
+
+    @Rule
+    @JvmField
+    val rxTestRule = RxImmediateSchedulerRule()
+
+    @Rule
+    @JvmField
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    lateinit var activity: FragmentActivity
+    lateinit var viewModel:DetailActivityViewModel
+    val  homeID = "133604"
+    val  awayID = "133602"
+
+    @Before
+    override fun setup(){
+        super.setup()
+        activity = Robolectric.setupActivity(FragmentActivity::class.java)
+        viewModel = ViewModelProviders.of(activity, viewModelFactory)[DetailActivityViewModel::class.java]
+    }
+
+
+    @Test
+    fun checkHomeTeam(){
+        assertEquals(null, viewModel.getListHomeTeam().value, "home team still null")
+        viewModel.getHomeTeams(homeID)
+        assertNotEquals(null, viewModel.getListHomeTeam().value, "home team should be not null")
+        assertEquals(null, viewModel.getListAwayTeam().value, "away team shoule be null")
+        assertEquals(null, viewModel.getListTeamError().value, "error shoule be null")
+        assertNotEquals("", viewModel.getListHomeTeam().value!!.logo, "home team logo should not be null")
+    }
+
+    @Test
+    fun checkAwayTeam(){
+        assertEquals(null, viewModel.getListAwayTeam().value, "away team still null")
+        viewModel.getAwayTeams(awayID)
+        assertNotEquals(null, viewModel.getListAwayTeam().value, "away team should be not null")
+        assertEquals(null, viewModel.getListHomeTeam().value, "home team shoule be null")
+        assertEquals(null, viewModel.getListTeamError().value, "error shoule be null")
+        assertNotEquals("", viewModel.getListAwayTeam().value!!.logo, "away team logo should not be null")
+    }
+}
