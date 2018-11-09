26763a386c54f0fd190ef3e457b5ec7584893d13
adit <kulik2@ymail.com>
2018-11-09 16:15:13 +0700

    searchview on main activity toolbar

--- app/src/main/java/com/adit/footballclub/ui/activity/MainActivity.kt
+++ app/src/main/java/com/adit/footballclub/ui/activity/MainActivity.kt
@@ -3,6 +3,7 @@ package com.adit.footballclub.ui.activity
 import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import dagger.android.AndroidInjection
 import kotlinx.android.synthetic.main.activity_main.*
 import androidx.navigation.Navigation
@@ -11,9 +12,12 @@ import com.adit.footballclub.R
 import com.adit.footballclub.viewmodel.EventViewModel
 import com.adit.footballclub.viewmodel.ViewModelFactory
 import javax.inject.Inject
+import android.support.v4.view.MenuItemCompat
+import android.support.v7.widget.SearchView
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),SearchView.OnQueryTextListener {
+
 
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
@@ -37,6 +41,22 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         eventViewModel.disposeElements()
         super.onDestroy()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.search_menu, menu)
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = MenuItemCompat.getActionView(searchItem) as SearchView
+        searchView.setOnQueryTextListener(this)
+        return true
+    }
+
+    override fun onQueryTextSubmit(p0: String?): Boolean {
+        return true
+    }
+
+    override fun onQueryTextChange(p0: String?): Boolean {
+        return true
     }
 
 }
