baeb5415e10c2e84603f8a1e629b197a37e60a43
adit <kulik2@ymail.com>
2018-11-12 13:38:39 +0700

    add notification for calendar

--- app/src/main/java/com/adit/footballclub/adapter/EventAdapter.kt
+++ app/src/main/java/com/adit/footballclub/adapter/EventAdapter.kt
@@ -1,9 +1,11 @@
 package com.adit.footballclub.adapter
 
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.adit.footballclub.ui.activity.DetailActivity
 import com.adit.footballclub.R
 import com.adit.footballclub.utils.Const
@@ -11,6 +13,13 @@ import com.adit.footballclub.utils.Utils
 import com.adit.footballclub.entity.Events
 import kotlinx.android.synthetic.main.match_item.view.*
 import org.jetbrains.anko.*
+import android.support.v4.content.ContextCompat.startActivity
+import android.provider.CalendarContract
+import org.robolectric.shadows.ShadowBuild.setType
+import android.content.Intent
+import android.R
+
+
 
 class EventAdapter(val eventList:List<Events>):RecyclerView.Adapter<EventAdapter.ViewHolder>(){
 
@@ -35,6 +44,22 @@ class EventAdapter(val eventList:List<Events>):RecyclerView.Adapter<EventAdapter
 
             itemView.setOnClickListener {
                 itemView.context.startActivity<DetailActivity>(Const.event to event)
+            }
+            itemView.btn_calendar.setOnClickListener {
+                val calIntent = Intent(Intent.ACTION_INSERT)
+                calIntent.type = "vnd.android.cursor.item/event"
+                calIntent.putExtra(Events.TITLE, "My House Party")
+                calIntent.putExtra(Events.EVENT_LOCATION, "My Beach House")
+                calIntent.putExtra(Events.DESCRIPTION, "A Pig Roast on the Beach")
+
+                val calDate = GregorianCalendar(2018,10,12)
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, true)
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+                        calDate.getTimeInMillis())
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+                        calDate.getTimeInMillis())
+
+                itemView.context.startActivity(calIntent)
             }
         }
 
